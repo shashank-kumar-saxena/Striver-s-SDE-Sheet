329583ca52f06a8a4315ca14275f3baf04fc81a7
Shashank <shashanksaxena226@gmail.com>
2022-05-03 14:35:26 +0530

    O(n) complexity

---
+++ Linked-List/IntersectionOfTwoLinkedList/src/com/shsnk/intersection/Main.java
@@ -0,0 +1,24 @@
+package com.shsnk.intersection;
+/*
+*   Example 1:
+    Input:
+    List 1 = [1,3,1,2,4], List 2 = [3,2,4]
+    Output:
+    2
+    Explanation: Here, both lists intersecting nodes start from node 2.
+
+*
+* Example 2:
+  Input:
+  List1 = [1,2,7], List 2 = [2,8,1]
+  Output:
+  Null
+  Explanation: Here, both lists do not intersect and thus no intersection node is present.
+* */
+//solve using utility function hashset
+//o(n+m) can achieve only using collection library
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
