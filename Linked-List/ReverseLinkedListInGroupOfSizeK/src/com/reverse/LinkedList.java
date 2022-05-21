d471fa210a82917f922dafa66fe2a54ca164d4b2
Shashank <shashanksaxena226@gmail.com>
2022-05-21 21:47:59 +0530

    new program

--- Linked-List/ReverseLinkedListInGroupOfSizeK/src/com/reverse/LinkedList.java
+++ Linked-List/ReverseLinkedListInGroupOfSizeK/src/com/reverse/LinkedList.java
@@ -10,4 +10,23 @@ public class LinkedList {
         head=temp;
         System.out.println("Node Insert Successfully");
     }
+    void reverse(int n)
+    {
+        int t=n;
+        temp=head;
+        Node start=head;
+        while (t>0&&temp.next!=null)
+        {
+            t--;
+            if(t==0)
+            {
+                while (start!=temp)
+                {
+
+                }
+                System.out.println();
+            }
+            temp=temp.next;
+        }
+    }
 }
--- Linked-List/ReverseLinkedListInGroupOfSizeK/src/com/reverse/Main.java
+++ Linked-List/ReverseLinkedListInGroupOfSizeK/src/com/reverse/Main.java
@@ -5,14 +5,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n
+        int n;
         while (true)
         {
             System.out.println("Enter your Choice");
             System.out.println("1 for insert an element");
             System.out.println("2 for reverse linked list in group of size k");
             System.out.println("3 for exit");
+            n=sc.nextInt();
+            switch (n){
+                case 1:
 
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+                default:
+                    System.out.println("Enter Valid Choice");
+            }
         }
     }
 }
